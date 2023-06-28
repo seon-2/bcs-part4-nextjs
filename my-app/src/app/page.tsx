dec1c9375e4b4471843dda4c84f8d1c57b13ecef
seon-2 <phia130@naver.com>
2023-06-28 09:35:32 +0900

    ✨ [feature] .env 변수 적용

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -3,13 +3,14 @@ import Image from "next/image";
 
 const Home: NextPage = () => {
   return (
-    <div className="bg-blue-300 min-h-screen flex justify-center items-center">
+    <div className="bg-blue-300 min-h-screen flex justify-center items-center flex-col p-24">
       <Image
         src="/images/rainbow.jpg"
         width={300}
         height={200}
         alt="무지개 배경"
       />
+      <div>{process.env.NEXT_JS}</div>
     </div>
   );
 };
