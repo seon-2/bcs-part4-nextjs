ba25c8bfface22bc815a07ce4b2a3c3da57c7993
seon-2 <phia130@naver.com>
2023-06-28 09:47:50 +0900

    ✨ [feature] 민팅 페이지 만들고 라우팅

---
+++ my-app/src/app/mint/page.tsx
@@ -0,0 +1,8 @@
+import { NextPage } from "next";
+// next 내장 페이지 라우팅. http://localhost:3000/mint 로 들어가서 확인
+// 폴더명이 경로가 됨 (폴더명은 소문자가 컨벤션)
+const Mint: NextPage = () => {
+  return <div>Mint</div>;
+};
+
+export default Mint;
