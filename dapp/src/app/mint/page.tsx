ac4298ccbbed76f3cbacbec0f2e39f8185537d7a
seon-2 <phia130@naver.com>
2023-06-29 13:36:14 +0900

    ✨ [feature] 민팅 기능 구현

--- dapp/src/app/mint/page.tsx
+++ dapp/src/app/mint/page.tsx
@@ -1,9 +1,27 @@
 "use client";
-
+import { useContext } from "react";
+import { mintNftContract } from "../lib/web3.config";
 import { NextPage } from "next";
+import { AppContext } from "../layout";
 
 const Mint: NextPage = () => {
-  return <div>Mint</div>;
+  const { account } = useContext(AppContext);
+
+  const onClickMint = async () => {
+    try {
+      const response = await mintNftContract.methods
+        .mintNft()
+        .send({ from: account });
+
+      console.log(typeof response.status);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <div>{account && <button onClick={onClickMint}>민팅하기</button>}</div>
+  );
 };
 
 export default Mint;
