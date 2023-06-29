14bf27c5883d6670d540dc4fe3dbd71ca1073def
seon-2 <phia130@naver.com>
2023-06-29 11:52:14 +0900

    🔧 [config] 컨트랙트 사용하기 위한 web3 설정

--- dapp/src/app/lib/web3.config.ts
+++ dapp/src/app/lib/web3.config.ts
@@ -1,4 +1,21 @@
 import { MetaMaskSDK } from "@metamask/sdk";
+import Web3 from "web3";
+import MINT_NFT_ABI from "../lib/mintNftAbi.json";
+import SALE_NFT_ABI from "../lib/saleNftAbi.json";
 
 const MMSDK = new MetaMaskSDK();
 export const ethereum = MMSDK.getProvider();
+
+const web3 = new Web3(ethereum);
+
+const MINT_NFT_ADDRESS = "0xE07675529794B77B193a42eb1375b6d8b4fAeA87";
+const SALE_NFT_ADDRESS = "0x8aED796D37363A5bE66446E1e1c0b8904f8e473B";
+
+export const mintNftContract = new web3.eth.Contract(
+  MINT_NFT_ABI,
+  MINT_NFT_ADDRESS
+);
+export const saleNftContract = new web3.eth.Contract(
+  SALE_NFT_ABI,
+  SALE_NFT_ADDRESS
+);
