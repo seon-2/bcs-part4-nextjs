417e1b65a97d10a04b68403ece82c5dfc34ef24d
seon-2 <phia130@naver.com>
2023-06-29 11:52:55 +0900

    🌷 [explain] Mint페이지에서 컨트랙트 상호작용 확인

--- dapp/src/app/mint/page.tsx
+++ dapp/src/app/mint/page.tsx
@@ -1,7 +1,24 @@
+"use client";
+
+import { mintNftContract } from "../lib/web3.config";
 import { NextPage } from "next";
 
 const Mint: NextPage = () => {
-  return <div>Mint</div>;
+  const onClickContract = async () => {
+    try {
+      const response = await mintNftContract.methods.name().call();
+
+      console.log(response);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <div>
+      Mint<button onClick={onClickContract}>컨트랙트 조회</button>
+    </div>
+  );
 };
 
 export default Mint;
