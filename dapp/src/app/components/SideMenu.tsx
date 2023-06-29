cc429451e86ff3bd36e94821926f88af7b335962
seon-2 <phia130@naver.com>
2023-06-29 10:17:54 +0900

    💄 [design] 사이드메뉴에 제목, 페이지 이동 버튼, footer 추가

--- dapp/src/app/components/SideMenu.tsx
+++ dapp/src/app/components/SideMenu.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import { FC, ReactNode } from "react";
 
 interface SideMenuProps {
@@ -8,7 +9,15 @@ const SideMenu: FC<SideMenuProps> = ({ children }) => {
   return (
     <div className="bg-red-100 min-h-screen flex">
       {/* shrink-0 옵션 줘야 w-full로 인해 줄어드는 것 막을 수 있음 */}
-      <nav className="bg-yellow-100 w-60 shrink-0">SideMenu</nav>
+      <nav className="bg-yellow-100 w-60 shrink-0 flex flex-col py-8 pl-4">
+        <div>프로젝트 다덴부</div>
+        <div className="bg-orange-100 grow">
+          <Link href="/">
+            <button>Main페이지</button>
+          </Link>
+        </div>
+        <div>Created by jiseon</div>
+      </nav>
       <div className="flex flex-col w-full">{children}</div>
     </div>
   );
