ff9fbe1deccc8d6ca40b8dd6fa1cbaee3e364b71
seon-2 <phia130@naver.com>
2023-06-29 13:21:34 +0900

    🔥 [remove] Mint페이지 상호작용 확인 후 코드 삭제

--- dapp/src/app/mint/page.tsx
+++ dapp/src/app/mint/page.tsx
@@ -1,24 +1,9 @@
 "use client";
 
-import { mintNftContract } from "../lib/web3.config";
 import { NextPage } from "next";
 
 const Mint: NextPage = () => {
-  const onClickContract = async () => {
-    try {
-      const response = await mintNftContract.methods.name().call();
-
-      console.log(response);
-    } catch (error) {
-      console.error(error);
-    }
-  };
-
-  return (
-    <div>
-      Mint<button onClick={onClickContract}>컨트랙트 조회</button>
-    </div>
-  );
+  return <div>Mint</div>;
 };
 
 export default Mint;
