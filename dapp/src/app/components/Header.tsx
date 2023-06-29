3cfb022b6637f7e80051d8a192fb786db5a80e58
seon-2 <phia130@naver.com>
2023-06-29 10:52:12 +0900

    ✨ [feature] 메타마스크 sdk 사용해 지갑로그인 기능 구현

--- dapp/src/app/components/Header.tsx
+++ dapp/src/app/components/Header.tsx
@@ -1,9 +1,31 @@
-import { FC } from "react";
+"use client";
+// 클라이언트쪽에서 실행되어야 할 때 상단에 적어줌
+// (nextjs는 서버프레임워크이기 때문에 서버에서 실행되어야 할 파일과 클라이언트에서 실행되어야 하는 파일을 구분해줘야 함)
+
+import { FC, useState } from "react";
+import { MetaMaskSDK } from "@metamask/sdk";
 
 const Header: FC = () => {
+  const [account, setAccount] = useState<string>("");
+
+  const MMSDK = new MetaMaskSDK();
+  const ethereum = MMSDK.getProvider();
+
+  const onClickLogIn = async () => {
+    try {
+      const accounts = await ethereum?.request({
+        method: "eth_requestAccounts",
+      });
+      setAccount(accounts[0]);
+      //   console.log(accounts);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <div className="bg-green-100 py-4 pr-8 flex justify-end">
-      <button>지갑로그인</button>
+      {account ? account : <button onClick={onClickLogIn}>지갑로그인</button>}
     </div>
   );
 };
