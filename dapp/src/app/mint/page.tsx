b8e281b263e3d520fbb5958719b7244504a1cd7c
seon-2 <phia130@naver.com>
2023-06-29 14:31:37 +0900

    ✨ [feature] 민팅한 nft 보여주는 기능 구현
    (오류 발생 - next config 설정 필요)
    다음 커밋에서 해결

--- dapp/src/app/mint/page.tsx
+++ dapp/src/app/mint/page.tsx
@@ -1,20 +1,20 @@
 "use client";
-import { useContext } from "react";
-import { mintNftContract, PINATA_URL } from "../lib/web3.config";
+import { useContext, useState } from "react";
+import { INft, mintNftContract, PINATA_URL } from "../lib/web3.config";
 import { NextPage } from "next";
 import { AppContext } from "../layout";
 import axios from "axios";
+import Image from "next/image";
 
 const Mint: NextPage = () => {
   const { account } = useContext(AppContext);
+  const [myNewNft, setMyNewNft] = useState<INft>();
 
   const onClickMint = async () => {
     try {
       const mintResponse = await mintNftContract.methods
         .mintNft()
         .send({ from: account });
-
-      // console.log(typeof response.status);
 
       if (Number(mintResponse.status) === 1) {
         const myNftResponse = await mintNftContract.methods
@@ -25,9 +25,14 @@ const Mint: NextPage = () => {
           `${PINATA_URL}/${Number(myNftResponse)}.json`
         );
 
-        console.log(metadataResponse);
+        // console.log(metadataResponse);
 
-        // console.log(myNftResponse);
+        setMyNewNft({
+          name: metadataResponse.data.name,
+          description: metadataResponse.data.description,
+          image: metadataResponse.data.image,
+          attributes: metadataResponse.data.attributes,
+        });
       }
     } catch (error) {
       console.error(error);
@@ -47,11 +52,27 @@ const Mint: NextPage = () => {
   return (
     <div>
       <div>{account && <button onClick={onClickMint}>민팅하기</button>}</div>
-      <div>
-        {account && (
-          <button onClick={onClickBalanceOf}>nft 개수 확인하기</button>
-        )}
-      </div>
+      {account && <button onClick={onClickBalanceOf}>nft 개수 확인하기</button>}
+      {myNewNft && (
+        <div>
+          <Image
+            src={myNewNft.image}
+            width={200}
+            height={200}
+            alt="NFT"
+            loading="lazy"
+          />
+          <div>이름 : {myNewNft.name}</div>
+          <div>설명 : {myNewNft.description}</div>
+          {myNewNft.attributes.map((v, i) => {
+            return (
+              <div key={i}>
+                {v.trait_type} : {v.value}
+              </div>
+            );
+          })}
+        </div>
+      )}
     </div>
   );
 };
