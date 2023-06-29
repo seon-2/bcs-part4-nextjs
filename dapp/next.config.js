5c67c479bd91a90a67631ed9527baccc1b0520c3
seon-2 <phia130@naver.com>
2023-06-29 14:32:07 +0900

    🔧 [config] images 구성요소 작성

--- dapp/next.config.js
+++ dapp/next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "olbm.mypinata.cloud",
+        port: "",
+        pathname: "/ipfs/**",
+      },
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
