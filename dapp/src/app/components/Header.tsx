3d615d513cb9c9bb5f534035494dfbfc5b3822c7
seon-2 <phia130@naver.com>
2023-06-29 10:25:55 +0900

    💄 [design] 지갑 로그인 버튼 만들기

--- dapp/src/app/components/Header.tsx
+++ dapp/src/app/components/Header.tsx
@@ -1,7 +1,11 @@
 import { FC } from "react";
 
 const Header: FC = () => {
-  return <div className="bg-green-100">Header</div>;
+  return (
+    <div className="bg-green-100 py-4 pr-8 flex justify-end">
+      <button>지갑로그인</button>
+    </div>
+  );
 };
 
 export default Header;
