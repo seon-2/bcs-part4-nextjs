93e58e785b3a509e1d1fa862aad60c8a768579c5
seon-2 <phia130@naver.com>
2023-06-30 11:43:28 +0900

    ✨ [feature] 판매상태 보여주고 변경하는 기능 구현

--- dapp/src/app/my-nft/page.tsx
+++ dapp/src/app/my-nft/page.tsx
@@ -12,6 +12,7 @@ const MyNft: NextPage = () => {
   // solidity의 getAllNft 함수 사용 (주소값 넣고 uint[] 반환함)
   const { account } = useContext(AppContext);
   const [tokenIds, setTokenIds] = useState<number[]>();
+  const [saleStatus, setSaleStatus] = useState<boolean>(false);
 
   const getMyNfts = async () => {
     try {
@@ -32,11 +33,27 @@ const MyNft: NextPage = () => {
   // 판매상태 가져오는 함수
   const getSaleStatus = async () => {
     try {
-      const response = await mintNftContract.methods
+      const response: boolean = await mintNftContract.methods
         .isApprovedForAll(account, SALE_NFT_ADDRESS)
         .call();
 
-      console.log(response);
+      setSaleStatus(response);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  // 판매상태 변경 함수 mint 컨트랙트의 setApprovalForAll 함수 이용
+  const onClickSaleStatus = async () => {
+    try {
+      const response = await mintNftContract.methods
+        .setApprovalForAll(SALE_NFT_ADDRESS, !saleStatus)
+        .send({ from: account });
+
+      // 정상일 경우 1 확인해서 state 변경(화면에 바로 적용)
+      if (Number(response.status) === 1) {
+        setSaleStatus(!saleStatus);
+      }
     } catch (error) {
       console.error(error);
     }
@@ -54,6 +71,16 @@ const MyNft: NextPage = () => {
 
   return (
     <div>
+      <div className="mb-20">
+        판매상태 : {saleStatus ? "승인" : "거부"}
+        <button
+          onClick={onClickSaleStatus}
+          className="border-2 border-black p-1"
+        >
+          판매상태변경
+        </button>
+      </div>
+
       {/* tokenIds 배열을 돌면서 NftCard 최신순으로(reverse)출력. key 설정 필요(index). props로 tokenId 보냄. */}
       {tokenIds?.reverse().map((v, i) => {
         return <NftCard key={i} tokenId={v} />;
