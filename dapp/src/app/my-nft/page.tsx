d2823f58b9720d510594b11a4a903e21945196b8
seon-2 <phia130@naver.com>
2023-06-30 09:41:38 +0900

    ✨ [feature] 내가 가진 nft 보여주는 기능 구현
    지갑 로그인 안 한 상태에서 오류 발생 (다음 커밋에서 해결)

--- dapp/src/app/my-nft/page.tsx
+++ dapp/src/app/my-nft/page.tsx
@@ -1,6 +1,29 @@
+"use client";
+
+import { mintNftContract } from "../lib/web3.config";
 import { NextPage } from "next";
+import { useContext, useEffect } from "react";
+import { AppContext } from "../layout";
 
 const MyNft: NextPage = () => {
+  // 내가 가진 nft 보여주는 기능
+  // solidity의 getAllNft 함수 사용 (주소값 넣고 uint[] 반환함)
+  const { account } = useContext(AppContext);
+
+  const getMyNfts = async () => {
+    try {
+      const response = await mintNftContract.methods.getAllNft(account).call();
+
+      console.log(response);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    getMyNfts();
+  }, []);
+
   return <div>MyNft</div>;
 };
 
