72179ec4f061a5f1a4a6e30e333ebd58fef82e7f
seon-2 <phia130@naver.com>
2023-07-05 10:31:46 +0900

    💄 [design] 판매등록 토글버튼 만들기

--- dapp/src/app/components/MyNtfCard.tsx
+++ dapp/src/app/components/MyNtfCard.tsx
@@ -8,6 +8,7 @@ interface MyNftCardProps {
 
 const MyNftCard: FC<MyNftCardProps> = ({ tokenId }) => {
   const [currentPrice, setCurrentPrice] = useState<number>(0);
+  const [saleToggle, setSaleToggle] = useState<boolean>(false);
 
   // nft 가격 받아오기
   const getCurrentPrice = async () => {
@@ -22,13 +23,30 @@ const MyNftCard: FC<MyNftCardProps> = ({ tokenId }) => {
     }
   };
 
+  // 판매등록 토글 버튼
+  const onClickSaleToggle = () => {
+    setSaleToggle(!saleToggle);
+  };
+
   useEffect(() => {
     getCurrentPrice();
   }, []);
 
   return (
     <div>
-      <div>현재 가격 : {currentPrice}Matic</div>
+      {currentPrice === 0 ? (
+        <>
+          <button onClick={onClickSaleToggle}>판매등록</button>
+          {saleToggle && (
+            <form>
+              <input type="text" />
+              <input type="submit" value="승인" />
+            </form>
+          )}
+        </>
+      ) : (
+        <div>현재 가격 : {currentPrice}Matic</div>
+      )}
       <NftCard tokenId={tokenId} />
     </div>
   );
