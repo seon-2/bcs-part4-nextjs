c83cca0a0ae1e3b2811d865d7e98c8a58016b180
seon-2 <phia130@naver.com>
2023-07-05 11:46:37 +0900

    ✨ [feature] nft 가격 가져와서 보여주기

--- dapp/src/app/components/SaleNftCard.tsx
+++ dapp/src/app/components/SaleNftCard.tsx
@@ -1,12 +1,37 @@
-import { FC } from "react";
+import { FC, useEffect, useState } from "react";
 import NftCard from "./NftCard";
+import { saleNftContract, web3 } from "../lib/web3.config";
 
 interface SaleNftCardProps {
   tokenId: number;
 }
 
 const SaleNftCard: FC<SaleNftCardProps> = ({ tokenId }) => {
-  return <NftCard tokenId={tokenId} />;
+  const [salePrice, setSalePrice] = useState<number>(0);
+
+  // nft 가격 가져오기
+  const getSalePrice = async () => {
+    try {
+      const response = await saleNftContract.methods
+        .getNftPrice(tokenId)
+        .call();
+
+      setSalePrice(Number(web3.utils.fromWei(Number(response), "ether")));
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    getSalePrice();
+  }, []);
+
+  return (
+    <div>
+      <div>{salePrice}Matic</div>
+      <NftCard tokenId={tokenId} />
+    </div>
+  );
 };
 
 export default SaleNftCard;
