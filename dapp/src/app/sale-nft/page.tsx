aa54d7d9a357d8cb7cbf5e1efb08628a48d41b8f
seon-2 <phia130@naver.com>
2023-07-05 11:28:33 +0900

    ✨ [feature] 판매중인 nft 가져와서 보여주기

--- dapp/src/app/sale-nft/page.tsx
+++ dapp/src/app/sale-nft/page.tsx
@@ -1,7 +1,39 @@
+"use client";
+
+import NftCard from "../components/NftCard";
+import { saleNftContract } from "../lib/web3.config";
 import { NextPage } from "next";
+import { useEffect, useState } from "react";
 
 const SaleNft: NextPage = () => {
-  return <div>SaleNft</div>;
+  const [saleNfts, setSaleNfts] = useState<number[]>();
+
+  // 판매중인 nft 가져오기
+  const getSaleNfts = async () => {
+    try {
+      const response: bigint[] = await saleNftContract.methods
+        .getOnSaleNft()
+        .call();
+
+      const tempArray = response.map((v) => Number(v));
+
+      setSaleNfts(tempArray);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    getSaleNfts();
+  }, []);
+
+  return (
+    <div>
+      {saleNfts?.map((v, i) => (
+        <NftCard key={i} tokenId={v} />
+      ))}
+    </div>
+  );
 };
 
 export default SaleNft;
