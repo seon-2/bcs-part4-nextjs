cb17d533ddd91776e438ef0c14112c94fa1ce171
seon-2 <phia130@naver.com>
2023-07-05 10:21:10 +0900

    ✨ [feature] nft 가격 가져오기

--- dapp/src/app/components/MyNtfCard.tsx
+++ dapp/src/app/components/MyNtfCard.tsx
@@ -1,11 +1,29 @@
-import { FC } from "react";
+import { FC, useEffect } from "react";
 import NftCard from "./NftCard";
+import { saleNftContract } from "../lib/web3.config";
 
 interface MyNftCardProps {
   tokenId: number;
 }
 
 const MyNftCard: FC<MyNftCardProps> = ({ tokenId }) => {
+    // nft 가격 받아오기
+  const getCurrentPrice = async () => {
+    try {
+      const response = await saleNftContract.methods
+        .getNftPrice(tokenId)
+        .call();
+
+      console.log(response); // 콘솔에서 0n으로 확인
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    getCurrentPrice();
+  }, []);
+
   return <NftCard tokenId={tokenId} />;
 };
 
