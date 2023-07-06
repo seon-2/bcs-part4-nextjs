c0a80e25cdac95d24fdda90d04ffcb26dcfa1727
seon-2 <phia130@naver.com>
2023-07-06 09:49:50 +0900

    ✨ [feature] sale 페이지에서 내가 가진 nft 확인하는 기능 구현
    내가 가진 nft 제외하고 구매하기 버튼 붙이기

--- dapp/src/app/components/SaleNftCard.tsx
+++ dapp/src/app/components/SaleNftCard.tsx
@@ -1,6 +1,7 @@
-import { FC, useEffect, useState } from "react";
+import { FC, useContext, useEffect, useState } from "react";
 import NftCard from "./NftCard";
-import { saleNftContract, web3 } from "../lib/web3.config";
+import { mintNftContract, saleNftContract, web3 } from "../lib/web3.config";
+import { AppContext } from "../layout";
 
 interface SaleNftCardProps {
   tokenId: number;
@@ -8,6 +9,8 @@ interface SaleNftCardProps {
 
 const SaleNftCard: FC<SaleNftCardProps> = ({ tokenId }) => {
   const [salePrice, setSalePrice] = useState<number>(0);
+  const [isMyNft, setIsMyNft] = useState<boolean>(false);
+  const { account } = useContext(AppContext);
 
   // nft 가격 가져오기
   const getSalePrice = async () => {
@@ -22,13 +25,46 @@ const SaleNftCard: FC<SaleNftCardProps> = ({ tokenId }) => {
     }
   };
 
+  // 내가 가진 nft인지 판별하는 함수. 내꺼 빼고 구매하기 버튼 붙이기
+  const getIsMyNft = async () => {
+    try {
+      const response: string = await mintNftContract.methods
+        .ownerOf(tokenId)
+        .call();
+
+      // 메타마스크로 가져온 주소와 web3로 가져온 주소가 대소문자가 달라서 맞춰줘야 함
+      // console.log("Sale Address : ", response, " My Address : ", account);
+      // console.log(response.toLowerCase() === account);
+      setIsMyNft(response.toLowerCase() === account);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   useEffect(() => {
     getSalePrice();
   }, []);
 
+  // account 변경될 때 getIsMyNft() 실행
+  useEffect(() => {
+    getIsMyNft();
+  }, [account]);
+
   return (
     <div>
       <div>{salePrice}Matic</div>
+      {isMyNft ? (
+        // 내 nft에 붙일 부분
+        <div>(My NFT)</div>
+      ) : (
+        // 지갑 로그인 되어있을 때만 구매 가능하도록
+        account && (
+          // 다른 사람들의 nft에 붙일 부분
+          <div>
+            <button>구매하기</button>
+          </div>
+        )
+      )}
       <NftCard tokenId={tokenId} />
     </div>
   );
