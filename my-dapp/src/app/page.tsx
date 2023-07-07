3c957efaee18df9fa0ec6d9893ce4290e4dd545a
seon-2 <phia130@naver.com>
2023-07-07 11:34:35 +0900

    ✨ [feature] 서명 요청하고 post에 같이 보내기

--- my-dapp/src/app/page.tsx
+++ my-dapp/src/app/page.tsx
@@ -3,6 +3,7 @@
 import axios from "axios";
 import { NextPage } from "next";
 import { FormEventHandler, useEffect, useState } from "react";
+import { personal } from "./lib/client";
 
 const Home: NextPage = () => {
   const [email, setEmail] = useState<string>("");
@@ -23,10 +24,20 @@ const Home: NextPage = () => {
 
       // 메타마스크 로그인 해서 지갑주소 있으면
       if (accounts) {
+        // 서명 요청
+        // 첫번째 값 data 중요! 한 글자라도 틀리면 다른 값이 나옴
+        const signedToken = await personal.sign("Hello", accounts[0], "Pass");
+        // console.log(signedToken);
+
+        // 서명된 값 기준으로 지갑주소 꺼내오기 (복호화)
+        const recoverAccount = await personal.ecRecover("Hello", signedToken);
+        console.log(recoverAccount);
+
         // axios로 POST 요청 보내기
         const response = await axios.post("http://localhost:3000/api/user", {
           account: accounts[0],
           email, // 키, 벨류가 이름이 같아 합쳐서
+          signedToken,
         });
         console.log(response);
       }
