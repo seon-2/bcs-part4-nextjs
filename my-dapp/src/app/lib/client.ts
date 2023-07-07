8dfc1d71a33bd77e3433d43e0b4339aa9a6fc4ff
seon-2 <phia130@naver.com>
2023-07-07 14:17:27 +0900

    🔧 [config] metamask SDK 적용

--- my-dapp/src/app/lib/client.ts
+++ my-dapp/src/app/lib/client.ts
@@ -1,4 +1,14 @@
+import { MetaMaskSDK } from "@metamask/sdk";
 import Web3, { Personal } from "web3";
 
-export const web3 = new Web3(window.ethereum);
-export const personal = new Personal(window.ethereum);
+const MMSDK = new MetaMaskSDK({
+  dappMetadata: {
+    name: "Project",
+    url: "https://project.project",
+  },
+});
+
+const ethereum = MMSDK.getProvider();
+
+export const web3 = new Web3(ethereum);
+export const personal = new Personal(ethereum);
