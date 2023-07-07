2cac2595f9d05cc23cba5e1cdd08586ced25b1cf
seon-2 <phia130@naver.com>
2023-07-07 10:05:23 +0900

    💄 [design] form으로 이메일 입력 받기

--- my-dapp/src/app/page.tsx
+++ my-dapp/src/app/page.tsx
@@ -1,7 +1,39 @@
+"use client";
+
 import { NextPage } from "next";
+import { FormEventHandler, useEffect, useState } from "react";
 
 const Home: NextPage = () => {
-  return <div>Home</div>;
+  const [email, setEmail] = useState<string>("");
+
+  // 이메일 입력 받아서 메타메스크로 로그인
+  const onSubmitMetamask: FormEventHandler = (e) => {
+    e.preventDefault();
+  };
+
+  // form 입력 확인
+  useEffect(() => {
+    console.log(email);
+  }, [email]);
+
+  return (
+    <div className="bg-red-100 min-h-screen p-24">
+      <div>
+        <form onSubmit={onSubmitMetamask}>
+          <input
+            type="text"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <input
+            className="ml-2 px-2 py-1 border-2 border-black rounded-md"
+            type="submit"
+            value="메타마스크 로그인"
+          />
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
