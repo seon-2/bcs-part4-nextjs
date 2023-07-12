1278b41688cd59b30e8c99c533a52eaf977902b3
seon-2 <phia130@naver.com>
2023-07-12 10:07:16 +0900

    🗃️ [DB] signedToken으로 user 가져와서 db에 userId 넣기

--- my-dapp/src/app/api/contract/route.ts
+++ my-dapp/src/app/api/contract/route.ts
@@ -6,11 +6,46 @@ export const POST = async (req: NextRequest) => {
   try {
     const body = await req.json();
 
-    const { address } = body;
+    const { address, signedToken } = body;
 
+    // signedToken 없는 경우 에러 처리 -> findFirst에서 undefined 걸러내지 못함
+    if (!signedToken) {
+      return NextResponse.json(
+        {
+          ok: false,
+          error: "Not exist token.",
+        },
+        {
+          status: 400,
+        }
+      );
+    }
+
+    // findFirst : unique 키워드는 없어서 여러 개가 조회될 수 있지만, 그 중 첫번째 값
+    const user = await prisma.user.findFirst({
+      where: {
+        signedToken,
+      },
+    });
+
+    // user 없는 경우 에러 처리
+    if (!user) {
+      return NextResponse.json(
+        {
+          ok: false,
+          error: "Not exist token.",
+        },
+        {
+          status: 400,
+        }
+      );
+    }
+
+    // user 있음
     const contract = await prisma.contract.create({
       data: {
         address,
+        userId: user.id,
       },
     });
 
